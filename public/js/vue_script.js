02429d29fa9c838a53e1069792356abc91818c8b
Fredrik Mattisson <fredrik.mattisson@hotmail.com>
2020-02-09 20:30:05 +0100

    map

--- public/js/vue_script.js
+++ public/js/vue_script.js
@@ -1,3 +1,5 @@
+'use strict';
+const socket = io();
 
 const vm_1 = new Vue({
     el: "#burgers",
@@ -13,19 +15,50 @@ const vm_2 = new Vue({
     el: "#order",
     data: {
 	order_placed: false,
+	orders: {},
 	this_order: [],
 	name: "",
 	email: "",
-	street: "",
-	house: null,
 	payment: "Credit card",
 	gender: "undisclosed"
+    },
+    created: function() {
+        socket.on('initialize',
+		  function(data) { this.orders = data.orders; }.bind(this)
+		 );
+
+	socket.on('currentQueue',
+		  function(data) { this.orders = data.orders; }.bind(this)
+		 );
     },
     methods: {
 	order: function() {
 	    this.order_placed = true;
 	    this.this_order = vm_1.selected;
 	    vm_1.selected = [];
+	},
+	getNext: function() {
+	    let lastOrder = Object.keys(this.orders).reduce(function(last, next) {
+		return Math.max(last, next); }, 0);
+
+	    return lastOrder + 1;
+	},
+	addOrder: function(event) {
+	    let offset = {
+		x: event.currentTarget.getBoundingClientRect().left,
+		y: event.currentTarget.getBoundingClientRect().top,
+	    };
+
+	    socket.emit('addOrder', {
+		orderId: this.getNext(),
+		details: {
+		    x: event.clientX - 10 - offset.x,
+		    y: event.clientY - 10 - offset.y,
+		},
+		orderItems: ['Beans', 'Curry'],
+	    });
 	}
     }
 })
+
+
